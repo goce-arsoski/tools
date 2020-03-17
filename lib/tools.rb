cf48f66843beeb59b1ffd0e5c5967066977ef29a
goce-arsoski <goce_ars@yahoo.com>
2020-03-17 01:05:16 +0100

    Luhn algorithm

--- lib/tools.rb
+++ lib/tools.rb
@@ -5,12 +5,53 @@ module Tools
     def initialize(str)
       @str = str
       @sum = 0
+      @double_every_second_array = []
+      @every_second_array_2 = []
     end
 
     def valid?
-      return false if @str.length <= 1
+      return false if @str.length <= 1 || @str.chars.first == ' '
 
-      true
+      stripped = @str.delete(' ')
+      return false if stripped.match(/["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]/)
+
+      return false if stripped.match(/["!", "\"", "#", "$", "£", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "{", "|", "}", "~"]/)
+
+      reversed_strip = stripped.reverse
+      reversed_array = reversed_strip.chars   # moze i so split('')
+      every_second_range = (1..reversed_array.length-1).step(2)
+      every_second_array = every_second_range.map { |i| reversed_array[i] }
+      every_second_array.map do |num|
+        a = num.to_i*2
+        if a >= 10
+          a -= 9
+        end
+
+        @double_every_second_array << a
+      end
+      every_second_range_2 = (0..reversed_array.length-1).step(2)
+      every_second_array_2 = every_second_range_2.map { |i| reversed_array[i] }
+      every_second_array_2.map do |num|
+        a = num.to_i
+        @every_second_array_2 << a
+      end
+      if sum % 10 == 0
+        return true
+      else
+        return false
+      end
+
+    end
+
+    private
+    def sum
+      @double_every_second_array.map do |num|
+        @sum += num
+      end
+      @every_second_array_2.map do |num|
+        @sum += num
+      end
+      return @sum
     end
   end
 end
--- spec/luhn_spec.rb
+++ spec/luhn_spec.rb
@@ -18,4 +18,61 @@ RSpec.describe Tools::Luhn do
     luhn = Tools::Luhn.new('059')
     expect(luhn.valid?).to be true
   end
+  it "becomes invalid if digits reversed." do
+    luhn = Tools::Luhn.new('59')
+    expect(luhn.valid?).to be true
+  end
+
+  it "is valid canadian sin num" do
+    luhn = Tools::Luhn.new('055 444 285')
+    expect(luhn.valid?).to be true
+  end
+
+  it "is invalid canadian sin num" do
+    luhn = Tools::Luhn.new('055 444 286')
+    expect(luhn.valid?).to be false
+  end
+
+  it "is invalid credit card" do
+    luhn = Tools::Luhn.new('8273 1232 7352 0569')
+    expect(luhn.valid?).to be false
+  end
+  it "becomes invalid with a non digit included" do
+    luhn = Tools::Luhn.new('055a 444 285')
+    expect(luhn.valid?).to be false
+  end
+
+  it "becomes invalid with a non digit added at the end" do
+    luhn = Tools::Luhn.new('059a')
+    expect(luhn.valid?).to be false
+  end
+  it "becomes invalid with punctuation included" do
+    luhn = Tools::Luhn.new('055-444-285')
+    expect(luhn.valid?).to be false
+  end
+
+  it "becomes invalid with symbols included" do
+    luhn = Tools::Luhn.new('055£ 444$ 285')
+    expect(luhn.valid?).to be false
+  end
+
+  it "is invalid single zero with space" do
+    luhn = Tools::Luhn.new(' 0')
+    expect(luhn.valid?).to be false
+  end
+
+  it "is valid more than a single zero" do
+    luhn = Tools::Luhn.new('0000 0')
+    expect(luhn.valid?).to be true
+  end
+
+  it "input digit 9 is correctly converted to output digit 9" do
+    luhn = Tools::Luhn.new('091')
+    expect(luhn.valid?).to be true
+  end
+
+  it "is invalid with non digits" do
+    luhn = Tools::Luhn.new(':9')
+    expect(luhn.valid?).to be false
+  end
 end
