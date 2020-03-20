2bc86a894efb5003a37f880a2857c6fc13922d30
goce-arsoski <goce_ars@yahoo.com>
2020-03-20 23:18:59 +0100

    Raindrops

--- lib/tools.rb
+++ lib/tools.rb
@@ -10,9 +10,9 @@ module Tools
     end
 
     def valid?
-      return false if @str.length <= 1 || @str.chars.first == ' '
-
       @stripped = @str.delete(' ')
+      return false if @stripped.length <= 1
+
       return false if @stripped.match(/["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]/)
 
       return false if @stripped.match(/["!", "\"", "#", "$", "£", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "{", "|", "}", "~"]/)
