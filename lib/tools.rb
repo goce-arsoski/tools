141e5015a51552a5cda9b4b150e897ce2699f519
goce-arsoski <goce_ars@yahoo.com>
2020-03-25 12:39:35 +0100

    Luhn final

--- lib/tools.rb
+++ lib/tools.rb
@@ -2,55 +2,45 @@ require "tools/version"
 
 module Tools
   class Luhn
-    LOWERCASE = ('a'..'z')
-    UPPERCASE = ('A'..'Z')
-
     def initialize(str)
-      @str = str
-      @double_every_second_array = []
-      @every_second_array_2 = []
+      @stripped = str.delete(' ')
     end
 
     def valid?
-      return false if @str.each_char.any?{ |char| LOWERCASE.cover?(char) || UPPERCASE.cover?(char) } # checking for letters in the string
+      return false if /\D/.match(@stripped)
 
-      @str[/\W/].nil? # checking for special characters in the string
+      return false if @stripped.length <= 1
 
-      @stripped = @str.delete(' ') # delete empty spaces from string
-      return false if @stripped.length <= 1 # checking for string length
-
-      (arrays_sum % 10).zero? ? true : false # checking validity for a card
+      (calculation % 10).zero?
     end
 
     private
 
-    def every_second_array
-      @reversed_strip = @stripped.reverse
-      @reversed_array = @reversed_strip.chars   # we can do this with split('')
-      every_second_range = (1..@reversed_array.length - 1).step(2)
-      every_second_array = every_second_range.map { |i| @reversed_array[i] }
-      every_second_array.map do |num|
-        a = num.to_i * 2
-        a -= 9 if a >= 10
-        @double_every_second_array << a
-      end
-
-      @double_every_second_array
+    def reversed_array
+      @stripped.reverse.chars
     end
 
-    def every_second_array_2
-      every_second_range_2 = (0..@reversed_array.length - 1).step(2)
-      every_second_array_2 = every_second_range_2.map { |i| @reversed_array[i] }
-      every_second_array_2.map do |num|
-        a = num.to_i
-        @every_second_array_2 << a
+    def checksum
+      every_second_digit_from_right.map do |num|
+        double = num * 2
+        double >= 10 ? double -= 9 : double
       end
-
-      @every_second_array_2
     end
 
-    def arrays_sum
-      every_second_array.sum + every_second_array_2.sum
+    def every_second_digit_from_right
+      indexes(1).map { |i| reversed_array[i].to_i }
+    end
+
+    def rest_of_the_elements
+      indexes.map { |i| reversed_array[i].to_i }
+    end
+
+    def indexes(idx = 0)
+      (idx..reversed_array.length - 1).step(2)
+    end
+
+    def calculation
+      (checksum + rest_of_the_elements).sum
     end
   end
 
