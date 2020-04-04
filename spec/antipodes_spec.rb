7bdcd89f0a97f3bf24fe6676719ed8661c11e0e4
goce-arsoski <goce_ars@yahoo.com>
2020-04-04 15:15:33 +0200

    Antipodes

--- spec/antipodes_spec.rb
+++ spec/antipodes_spec.rb
@@ -1,15 +1,15 @@
 RSpec.describe Tools::Antipodes do
-  it 'return corect result' do
+  it 'return correct result' do
     antipodes = Tools::Antipodes.new([1, 2, 3, 4])
     expect(antipodes.division_by_two).to eq [2.5, 2.5]
   end
 
-  it 'return corect result with odd number' do
+  it 'return correct result with odd number' do
     antipodes = Tools::Antipodes.new([1, 2, 3, 4, 5])
     expect(antipodes.division_by_two).to eq [3, 3]
   end
 
-  it 'return corect result with negative numbers ' do
+  it 'return correct result with negative numbers' do
     antipodes = Tools::Antipodes.new([-1, -2])
     expect(antipodes.division_by_two).to eq [-1.5]
   end
